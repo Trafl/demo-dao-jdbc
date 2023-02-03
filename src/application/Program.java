32918a51629f1aee0bef9ad989a4b05295d1443b
P.Ivo <pivolluz@gmail.com>
2023-02-03 18:36:06 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -39,5 +39,11 @@ public class Program {
 		Seller newSeller = new Seller(null, "greg", "greg@email.com", new Date(), 4000.0,department );
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! new id = " + newSeller.getId());
+		
+		System.out.println("=== TEST 5: Seller Update  ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Seller update!");
 	}
 }
